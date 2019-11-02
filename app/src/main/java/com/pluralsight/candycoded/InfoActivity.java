7f39225c16577a21e99bca22b9eb9afc7d4a00e8
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-11-02 15:38:13 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -31,6 +31,7 @@ public class InfoActivity extends AppCompatActivity {
     public void createMapIntent(View view) {
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
 
     }
 
